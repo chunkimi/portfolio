5de3afbcbe79dbe554588f937806fa2d4cd51d72
kimi <huchiachun@gmail.com>
2024-11-25 23:20:32 +0800

    refector(ResumeView) : refector resume layot and update info data

--- src/components/resume/section/WorkProj.vue
+++ src/components/resume/section/WorkProj.vue
@@ -5,6 +5,15 @@
     flex-direction: column;
     gap: 2.5rem;
   }
+  &__side {
+    &__img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      object-position: center center;
+      opacity: 0.75;
+    }
+  }
 }
 
 .work-proj-overview-collapse-enter-active,
@@ -21,10 +30,19 @@
   <div class="container">
     <div class="resume-section__wrap">
       <h3 class="resume-section__header">{{ awardsConfig.sectionTitle }}</h3>
-      <div class="work-proj__container">
-        <ProjAwards />
-        <ProjExpSummary :proj-summary-data="projExpSummaryData" />
-        <ProjHighlights :highlights-data="projHighlightsDate" />
+      <div class="row">
+        <div class="col-12 col-lg-10">
+          <div class="work-proj__container">
+            <ProjAwards />
+            <ProjHighlights :highlights-data="projHighlightsDate" />
+            <ProjExpSummary :proj-summary-data="projExpSummaryData" />
+          </div>
+        </div>
+        <div class="col-lg-2" v-if="isMediaLgUp">
+          <img :src="getImage(awardsConfig.sideImg)" alt="side-img" class="work-proj__side__img" />
+        </div>
+      </div>
+      <div class="my-10">
         <div class="d-flex justify-content-center">
           <button type="button" class="btn btn-outline-auxiliary" @click="toggleExtend">
             <span class="d-flex align-items-center">
@@ -33,21 +51,25 @@
             </span>
           </button>
         </div>
-        <transition name="work-proj-overview-collapse">
-          <ProjOverview :overview-data="projOverviewData" v-if="isExtend" />
-        </transition>
       </div>
+      <transition name="work-proj-overview-collapse">
+        <ProjOverview :overview-data="projOverviewData" v-if="isExtend" />
+      </transition>
     </div>
   </div>
 </template>
 <script setup>
 import { ref, computed } from 'vue'
+import { useMediaQuery } from '@vueuse/core'
 import { resumeNavMenu } from '@/utils/resumeUtils.js'
-
+import { getImage } from '@/utils/imgUtils'
+import { mediaBreakpointConfig } from '@/utils/mediaUtils.js'
 import ProjExpSummary from '@/components/resume/block/proj/ProjExpSummary.vue'
 import ProjAwards from '@/components/resume/block/proj/ProjAwards.vue'
 import ProjHighlights from '@/components/resume/block/proj/ProjHighlights.vue'
 import ProjOverview from '@/components/resume/block/proj/ProjOverview.vue'
+
+const isMediaLgUp = useMediaQuery(`(min-width: ${mediaBreakpointConfig.lg})`)
 
 const awardsConfig = {
   sectionTitle: resumeNavMenu.find((item) => item.sectionID === 'work-proj').title,
@@ -60,7 +82,8 @@ const awardsConfig = {
       less: 'keyboard_arrow_up',
       more: 'keyboard_arrow_down'
     }
-  }
+  },
+  sideImg: 'assets/images/proj-list.jpg'
 }
 
 const props = defineProps({
